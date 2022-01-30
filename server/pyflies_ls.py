1f9ff7feda6bbff805fc8c5e1395c2d23d73ea4f
Dejan <dejans1224@gmail.com>
2022-01-30 19:10:02 +0100

    Initial working implementation for Reference feature

--- server/pyflies_ls.py
+++ server/pyflies_ls.py
@@ -1,6 +1,6 @@
 import time
 from pygls.lsp.methods import (COMPLETION, DEFINITION, TEXT_DOCUMENT_DID_CHANGE,
-                               CODE_ACTION, TEXT_DOCUMENT_DID_OPEN)
+                               CODE_ACTION, TEXT_DOCUMENT_DID_OPEN, REFERENCES)
 
 # from pygls.capabilities import COMPLETION, TEXT_DOCUMENT_DID_CHANGE
 from typing import List, Optional, Union
@@ -8,12 +8,13 @@ from pygls.server import LanguageServer
 from pygls.lsp import (CompletionItem, CompletionList, CompletionOptions,
                        CompletionParams, DefinitionParams, DidChangeTextDocumentParams,
                        DidOpenTextDocumentParams, CodeActionOptions, CodeActionKind,
-                       CodeActionParams, CodeAction, Command, Location)
+                       CodeActionParams, CodeAction, Command, ReferenceParams)
 from pygls.workspace import Document
 from .features.validate import validate
 from .features.completion import process_completions, trigger_characters
 from .features.code_actions import process_quick_fix
 from .features.definitions import resolve_definition
+from .features.references import resolve_references
 from .util import load_document, get_entire_string_from_index, load_document_source
 
 COUNT_DOWN_START_IN_SECONDS = 12
@@ -58,6 +59,13 @@ def definitions(ls, params: DefinitionParams):
     name = get_entire_string_from_index(text_doc.offset_at_position(params.position), text_doc.source)
     defs = resolve_definition(text_doc.source, name, params.text_document.uri)
     return [defs]
+
+@pyflies_server.feature(REFERENCES)
+def references(ls, params: ReferenceParams):
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+    name = get_entire_string_from_index(text_doc.offset_at_position(params.position), text_doc.source)
+    refs = resolve_references(text_doc.source, name, params.text_document.uri)
+    return refs
 
 @pyflies_server.feature(CODE_ACTION, CodeActionOptions(code_action_kinds=[CodeActionKind.Refactor]))
 def code_actions(ls, params: CodeActionParams) -> Optional[List[Union[Command, CodeAction]]]:
